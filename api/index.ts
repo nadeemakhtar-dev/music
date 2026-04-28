7665b86de9cfe667ceda491d999d80d61144fb03
nadeem akhtar <nadeemakhtar@nadeems-MacBook-Air.local>
2026-04-28 10:18:54 +0530

    fix hono vercel handler

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,9 @@
 import { AlbumController, ArtistController, SearchController, SongController } from '../dist/modules/index.js'
 import { PlaylistController } from '../dist/modules/playlists/controllers/index.js'
 import { App } from '../dist/app.js'
+import { handle } from 'hono/vercel' // 👈 IMPORTANT
 
-const app = new App([
+const honoApp = new App([
   new SearchController(),
   new SongController(),
   new AlbumController(),
@@ -10,4 +11,4 @@ const app = new App([
   new PlaylistController()
 ]).getApp()
 
-export default app
+export default handle(honoApp)
